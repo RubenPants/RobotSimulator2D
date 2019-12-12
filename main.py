56229dab95d29f10042c991ff979ac9288f27324
RubenPants <broekxruben@gmail.com>
2019-12-12 12:10:37 +0100

    Evaluated each of the populations

--- main.py
+++ main.py
@@ -29,12 +29,12 @@ def query_net(net, states):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--train', type=bool, default=False)
-    parser.add_argument('--evaluate', type=bool, default=True)
-    parser.add_argument('--visualize', type=bool, default=False)
+    parser.add_argument('--evaluate', type=bool, default=False)
+    parser.add_argument('--visualize', type=bool, default=True)
     args = parser.parse_args()
     
     pop = Population(
-            name='path_time',
+            name='distance',
             rel_path='control/NEAT/',
             make_net_method=make_net,
             query_net_method=query_net,
