d24cd0065b1e667b2eb6cfe8aa148ec81193ea46
RubenPants <broekxruben@gmail.com>
2020-02-18 11:18:41 +0100

    Created blueprints

--- environment/game_visualizer.py
+++ environment/game_visualizer.py
@@ -4,6 +4,8 @@ game_visualizer.py
 This method is purely used to visualize the game-maps (used in the thesis). This file must be called in the environment
 folder to make sure the visualizations are saved properly.
 """
+import os
+
 import matplotlib as mpl
 import matplotlib.colors as clr
 import matplotlib.pyplot as plt
@@ -32,7 +34,7 @@ def game_blueprint(game):
     """
     game.get_blueprint()
     plt.title("Blueprint - Game {id:05d}".format(id=game.id))
-    plt.savefig('visualizations/blueprint_game{g:05d}'.format(g=game.id))
+    plt.savefig(f'environment/visualizations/blueprint_game{game.id:05d}')
     plt.close()
 
 
@@ -66,10 +68,11 @@ def path_heatmap(game):
     mpl.colorbar.ColorbarBase(cax, cmap=clr.ListedColormap(data), norm=norm, orientation='vertical')
     
     # Save the figure
-    plt.savefig('visualizations/heatmap_game{g:05d}'.format(g=game.id))
+    plt.savefig(f'environment/visualizations/heatmap_game{game.id:05d}')
 
 
 if __name__ == '__main__':
+    os.chdir("..")
     # Load the game
     for g_id in range(1, 11):
         g = load_game(g_id)
