6e5c6f008c0cd05a4c9cb98e604650822d965048
RubenPants <broekxruben@gmail.com>
2020-03-07 02:30:20 +0100

    Trained 6/8th

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     parser.add_argument('--iterations', type=int, default=1)
     parser.add_argument('--blueprint', type=bool, default=False)
     parser.add_argument('--evaluate', type=bool, default=False)
-    parser.add_argument('--genome', type=bool, default=False)
+    parser.add_argument('--genome', type=bool, default=True)
     parser.add_argument('--live', type=bool, default=True)
     args = parser.parse_args()
     
     pop = Population(
-            name="distance_1",
+            name="novelty_repr_1",
             # version=1,
-            folder_name='NEAT',
+            folder_name='NEAT-GRU',
     )
     if not pop.best_genome: pop.best_genome = list(pop.population.values())[0]
     # pop.population[9] = pop.population[list(pop.population.keys())[12]]
