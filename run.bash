cc0ae4ec991982148c02a0e3dd2505a0a584c788
RubenPants <broekxruben@gmail.com>
2020-03-13 21:04:49 +0100

    Update run.bash

--- run.bash
+++ run.bash
@@ -6,20 +6,20 @@ blueprint=1
 
 # Sexual GRU
 python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=1 --fitness=distance --blueprint=$blueprint;
-#python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=1 --fitness=path --blueprint=$blueprint;
+python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=1 --fitness=path --blueprint=$blueprint;
 python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=1 --fitness=novelty --blueprint=$blueprint;
 
 # Asexual GRU
 python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=1 --fitness=distance --blueprint=$blueprint;
-#python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=1 --fitness=path --blueprint=$blueprint;
+python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=1 --fitness=path --blueprint=$blueprint;
 python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=1 --fitness=novelty --blueprint=$blueprint;
 
 # Sexual non-GRU
 python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=0 --fitness=distance --blueprint=$blueprint;
-#python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=0 --fitness=path --blueprint=$blueprint;
+python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=0 --fitness=path --blueprint=$blueprint;
 python3 run_population.py --train=1 --iterations=$iter --reproduce=1 --enable_gru=0 --fitness=novelty --blueprint=$blueprint;
 
 # Asexual non-GRU
 python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=0 --fitness=distance --blueprint=$blueprint;
-#python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=0 --fitness=path --blueprint=$blueprint;
+python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=0 --fitness=path --blueprint=$blueprint;
 python3 run_population.py --train=1 --iterations=$iter --reproduce=0 --enable_gru=0 --fitness=novelty --blueprint=$blueprint;
