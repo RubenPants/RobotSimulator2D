46e31248295c0b5e58b38dd1a649e1ded5af8226
RubenPants <broekxruben@gmail.com>
2020-03-19 22:42:17 +0100

    Update run.bash

--- run.bash
+++ run.bash
@@ -8,25 +8,25 @@ eval=1;
 trace=1;
 
 # Sexual GRU
-#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=1 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=1 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=1 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=1 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=1 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 
 # Asexual GRU
-#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=1 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 
 # Sexual non-GRU
-python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=0 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=0 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=0 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=0 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 #python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=1 --gru_enabled=0 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
 
 # Asexual non-GRU
-python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
-python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=distance --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=diversity --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=novelty --blueprint=$blueprint --evaluate=$eval --trace=$trace;
+#python3 run_population.py --version=$version --train=1 --iterations=$iter --reproduce=0 --gru_enabled=0 --fitness=path --blueprint=$blueprint --evaluate=$eval --trace=$trace;
