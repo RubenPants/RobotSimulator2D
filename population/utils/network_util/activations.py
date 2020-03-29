5e3f7c7994e6a73db0752a932b470a6b6da3b129
RubenPants <broekxruben@gmail.com>
2020-03-29 13:40:59 +0200

    trained delta-distance v2 for 100gen

--- population/utils/network_util/activations.py
+++ population/utils/network_util/activations.py
@@ -5,36 +5,20 @@ Each of the supported activation functions.
 """
 import torch
 
-from utils.dictionary import D_CLAMPED_ABS, D_CLAMPED_LINEAR, D_COS, D_EXP, D_EXP_ABS, D_GAUSS, D_HAT, D_SIGMOID, D_SIN, \
-    D_TANH
+from utils.dictionary import D_CLAMPED_ABS, D_CLAMPED_LINEAR, D_COS, D_EXP, D_EXP_ABS, D_GAUSS, D_HAT, D_SIGMOID, \
+    D_SIN, D_TANH
 
 
-def sigmoid_activation(x):
-    return torch.sigmoid(3 * x)
-
-
-def tanh_activation(x):
-    return torch.tanh(1.5 * x)
-
-
-def gauss_activation(x):
-    return torch.exp(-2.0 * x ** 2)
-
-
-def sin_activation(x):
-    return torch.sin(x*3.14)
-
-
-def cos_activation(x):
-    return torch.cos(x*3.14)
+def clamped_abs_activation(x):
+    return min(1.0, torch.abs(x))
 
 
 def clamped_linear_activation(x):
     return max(-1.0, min(1.0, x))
 
 
-def clamped_abs_activation(x):
-    return min(1.0, torch.abs(x))
+def cos_activation(x):
+    return torch.cos(x * 3.14)
 
 
 def exponential_activation(x):
@@ -45,19 +29,35 @@ def exponential_abs_activation(x):
     return torch.abs(x) ** (1 / torch.abs(x))
 
 
+def gauss_activation(x):
+    return torch.exp(-2.0 * x ** 2)
+
+
 def hat_activation(z):
     return max(0.0, 1 - torch.abs(z))
 
 
+def sigmoid_activation(x):
+    return torch.sigmoid(3 * x)
+
+
+def sin_activation(x):
+    return torch.sin(x * 3.14)
+
+
+def tanh_activation(x):
+    return torch.tanh(1.5 * x)
+
+
 str_to_activation = {
-    D_SIGMOID:        sigmoid_activation,
-    D_TANH:           tanh_activation,
-    D_GAUSS:          gauss_activation,
-    D_SIN:            sin_activation,
-    D_COS:            cos_activation,
-    D_CLAMPED_LINEAR: clamped_linear_activation,
     D_CLAMPED_ABS:    clamped_abs_activation,
+    D_CLAMPED_LINEAR: clamped_linear_activation,
+    D_COS:            cos_activation,
     D_EXP:            exponential_activation,
     D_EXP_ABS:        exponential_abs_activation,
+    D_GAUSS:          gauss_activation,
     D_HAT:            hat_activation,
+    D_SIN:            sin_activation,
+    D_SIGMOID:        sigmoid_activation,
+    D_TANH:           tanh_activation,
 }
